bca0e5ef421666c5e088c2f08ae69afef4f98a7f
netinvman <linux-fox@163.com>
2015-04-08 10:21:40 +0800

    add insertStr.py

---
+++ insertStr.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+fr = open('d:/AAAA.txt', 'r')
+lineNum = 0
+fileList = fr.readlines()
+fr.close()
+for item in fileList:
+    if lineNum % 5 == 0:
+        fileList.insert(lineNum, 'AAAAAAAAAAAA\n')
+    lineNum += 1
+
+fw = open('d:/AAAA.txt', 'w')
+for item in fileList:
+    fw.write(item)
+    
+fw.close()
+        
+    
